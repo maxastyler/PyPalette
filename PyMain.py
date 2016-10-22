cede083043a2c15bee49277f1b4183ae80bb0c3d
Max Tyler <mtyler88@gmail.com>
2016-10-22 22:30:26 +0100

    First commit, little program to generate terminal themes from images

---
+++ PyMain.py
@@ -0,0 +1,229 @@
+import tkinter as tk
+import tkinter.colorchooser as tkc
+from tkinter import ttk
+from tkinter import filedialog
+import json
+import os
+from extractor import *
+from PIL import Image, ImageTk
+
+colour_labels=["colour_bg", "colour_fg"]
+for i in range(16):
+    colour_labels.append("colour_{0}".format(i))
+
+def load_json(file_name):
+    return json.load(file_name)
+
+class MainApp:
+    def __init__(self, root):
+        self.root=root
+        self.app=PyPallet(master=self.root)
+    def run(self):
+        self.app.mainloop()
+
+class PyPallet(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.pack(expand=True, fill='both')
+        self.create_widgets(master)
+
+    def create_widgets(self, master):
+        self.notebook=ttk.Notebook(self)
+        self.colours_page=ColoursPage(self)
+        #self.colours_render=ColourRenderer(self, self.colours_page)
+        self.notebook.add(self.colours_page, text="Colours Page")
+        self.notebook.add(ImageLoader(self, self.colours_page), text='Image Loader')
+        #self.notebook.add(self.colours_render, text="Colours Render")
+        self.notebook.pack(expand=True, fill='both')
+        #self.notebook.grid(row=0, column=0, sticky='news')
+        #self.notebook.rowconfigure(0, weight=1)
+        #self.notebook.columnconfigure(0, weight=1)
+
+class ColoursPage(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.load_text=tk.StringVar()
+        self.load_text.set(os.path.abspath('.')+"/")
+        self.create_widgets()
+
+    def create_widgets(self):
+        colours={}
+        self.labels={}
+        for colour in colour_labels:
+            self.labels[colour]=ColourPicker(self, colour, "#ffffff")
+        self.labels["colour_bg"].grid(row=0, column=0, sticky='news')
+        self.labels["colour_fg"].grid(row=0, column=1, sticky='news')
+        for i in range(16):
+            c=1
+            if i<8: c=0
+            r = i+1
+            if i>7: r=i-7
+            self.labels["colour_{0}".format(i)].grid(row=r, column=c, sticky='news')
+        
+        self.file_box=tk.Entry(self, textvariable=self.load_text)
+        self.file_box.grid(row=9, column=0, columnspan=2, sticky="news")
+        self.loadbutton=tk.Button(self, text="Load file", command=self.load_scheme)
+        self.loadbutton.grid(row=10, column=0)
+        self.savebutton=tk.Button(self, text="Save file", command=self.save_scheme)
+        self.savebutton.grid(row=10, column=1)
+        self.colour_renderer=ColourRenderer(self, self)
+        self.colour_renderer.grid(row=0, column=3, rowspan=11, sticky="news")
+        self.reset_button=tk.Button(self, text="Reset Colours", command=self.reset_colours)
+        self.reset_button.grid(row=12, column=3)
+        self.set_light=tk.Button(self, text="Set Lighter Colours to Darker", command=self.set_lighter_darker)
+        self.set_light.grid(row=13, column=3)
+        for i in range(11): 
+            self.rowconfigure(i, weight=1) 
+        for i in range(2):
+            self.columnconfigure(i, weight=1)
+
+    def load_scheme(self, *args):
+        cols=None
+
+        p=os.path.expanduser(self.load_text.get())
+        n=os.path.basename(p)
+        p=os.path.dirname(p)
+        with filedialog.askopenfile(initialdir=p, initialfile=n) as f:
+            cols=load_json(f)
+        if cols is not None:
+            for key in cols:
+                self.labels[key].colour.set(cols[key])
+        self.colour_renderer.update_self()
+
+    def save_scheme(self, *args):
+        pathname=os.path.expanduser(self.load_text.get())
+        pathstrip=os.path.dirname(pathname)
+        n=os.path.basename(pathname)
+        if os.path.exists(pathstrip):
+            cols={}
+            for key in self.labels:
+                cols[key]=self.labels[key].colour.get()
+            with filedialog.asksaveasfile(initialdir=pathstrip, initialfile=n) as f:
+                json.dump(cols, f)
+                print("Saved to {0}".format(pathname))
+
+    def reset_colours(self, *args):
+        for key in self.labels:
+            self.labels[key].colour.set("#ffffff")
+
+    def set_lighter_darker(self, *args):
+        for i in range(8):
+            self.labels['colour_{0}'.format(i+8)].colour.set(self.labels['colour_{0}'.format(i)].colour.get())
+
+class ColourPicker(tk.Frame):
+    def __init__(self, master, label, colour="#FFFFFF", *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.colour=tk.StringVar()
+        self.colour.set(colour)
+        self.create_widgets(label)
+        self.colour.trace('w', self.set_box_colour)
+        self.set_box_colour()
+        
+    def create_widgets(self, label):
+        self.label=tk.Label(self, text=label)
+        self.label.grid(row=0, column=0, sticky='nswe')
+        self.button=tk.Button(self, textvariable=self.colour)
+        self.button["command"]=self.colour_helper
+        self.text_box=tk.Entry(self, textvariable=self.colour)
+        self.button.grid(row=1, column=0, sticky="nswe")
+        self.text_box.grid(row=2, column=0, sticky='nswe')
+        self.columnconfigure(0, weight=1)
+        for i in range(3):
+            self.rowconfigure(i, weight=1)
+
+    def colour_helper(self):
+        try:
+            returncol=tkc.askcolor(self.colour.get())[1]
+        except:
+            returncol=tkc.askcolor('#ffffff')[1]
+        if returncol!=None:
+            self.colour.set(returncol)
+
+    def set_box_colour(self, *args):
+        try:
+            self.button["bg"]=self.colour.get()
+        except:
+            self.button["bg"]="#FFFFFF"
+
+class ColourRenderer(tk.Frame):
+    def __init__(self, master, colours_page, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.canvas_c=(500, 500)
+        self.colours_page=colours_page.labels
+        self.canvas=tk.Canvas(self, width=self.canvas_c[0], height=self.canvas_c[1], bg='#ffffff')
+        self.canvas.place()
+        self.canvas.grid(row=0, column=0, sticky='news')
+        self.button=tk.Button(self, text='Generate', command=self.update_self)
+        self.button.grid(row=1, column=0, sticky='news')
+        self.update_self()
+
+    def update_self(self):
+        self.canvas.delete('all')
+        box_height=self.canvas_c[1]/9.  
+        third_height=box_height/3.
+        box_width=self.canvas_c[0]/9.
+        half_width=box_width/2.
+
+        self.canvas.create_rectangle(0, 0, self.canvas_c[0], box_height, fill=self.colours_page['colour_bg'].colour.get())
+        bgcol=self.colours_page['colour_bg'].colour.get()
+        fgcol=self.colours_page['colour_fg'].colour.get()
+        self.canvas.create_text(half_width, third_height, text=bgcol, fill=bgcol)
+        self.canvas.create_text(half_width, 2*third_height, text=fgcol, fill=fgcol)
+        for j in range(8):
+            col_bg=self.colours_page['colour_{0}'.format(j)].colour.get()
+            col_fg=self.colours_page['colour_{0}'.format(j+8)].colour.get()
+            self.canvas.create_text((j+1)*box_width+half_width, third_height, text=col_bg, fill=col_bg)
+            self.canvas.create_text(half_width+box_width*(j+1), third_height*2, text=col_fg, fill=col_fg)
+
+        for i in range(8):
+            self.canvas.create_rectangle(0, box_height*(i+1), self.canvas_c[0], box_height*(i+2), fill=self.colours_page['colour_{0}'.format(i)].colour.get())
+            self.canvas.create_text(half_width, third_height+box_height*(i+1), text=bgcol, fill=bgcol)
+            self.canvas.create_text(half_width, third_height*2+box_height*(i+1), text=fgcol, fill=fgcol)
+            for j in range(8):
+                col_bg=self.colours_page['colour_{0}'.format(j)].colour.get()
+                col_fg=self.colours_page['colour_{0}'.format(j+8)].colour.get()
+                self.canvas.create_text((j+1)*box_width+half_width, third_height+box_height*(i+1), text=col_bg, fill=col_bg)
+                self.canvas.create_text(half_width+box_width*(j+1), third_height*2+box_height*(i+1), text=col_fg, fill=col_fg)
+
+class ImageLoader(tk.Frame):
+    def __init__(self, master, colours_page, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.cp=colours_page
+        self.load_text=tk.StringVar()
+        self.load_text.set(os.path.abspath('.')+"/")
+
+        self.create_widgets()
+
+
+    def create_widgets(self):
+        self.canvas=tk.Canvas(self, width=600, height=600)
+        self.entry=tk.Entry(self, textvariable=self.load_text)
+        self.load18=tk.Button(self, text="Load Image 18 colours", command=self.load_image_18)
+        self.load9=tk.Button(self, text="Load Image 9 colours", command=self.load_image_9)
+        self.canvas.grid(row=0, column=0, columnspan=1, sticky='news')
+        self.entry.grid(row=1, column=0, columnspan=1, sticky='news')
+        self.load18.grid(row=2, column=0)
+        self.load9.grid(row=3, column=0)
+
+    def load_image_18(self, *args):
+        pilimage=Image.open(os.path.expanduser(self.load_text.get()))
+        pilimage.thumbnail((600, 600))
+        self.photo=ImageTk.PhotoImage(pilimage)
+        self.canvas.delete('all')
+        self.canvas.create_image(300, 300, image=self.photo)
+        cols=prepare_pypallet(os.path.expanduser(self.load_text.get()))
+        for key in self.cp.labels:
+            self.cp.labels[key].colour.set(cols[key])
+
+    def load_image_9(self, *args):
+        pilimage=Image.open(os.path.expanduser(self.load_text.get()))
+        pilimage.thumbnail((600, 600))
+        self.photo=ImageTk.PhotoImage(pilimage)
+        self.canvas.delete('all')
+        self.canvas.create_image(300, 300, image=self.photo)
+        cols=prepare_pypallet_9(os.path.expanduser(self.load_text.get()))
+        for key in cols:
+            self.cp.labels[key].colour.set(cols[key])
+
+
---
+++ extractor.py
@@ -0,0 +1,204 @@
+from PIL import Image
+from skimage import color
+from clusterer import *
+import numpy as np
+
+orig_term_colours={
+    "color0":  "#000000",
+    "color8":  "#555753",
+    "color1":  "#ff6565",
+    "color9":  "#ff8d8d",
+    "color2":  "#93d44f",
+    "color10": "#c8e7a8",
+    "color3":  "#eab93d",
+    "color11": "#ffc123",
+    "color4":  "#204a87",
+    "color12": "#3465a4",
+    "color5":  "#ce5c00",
+    "color13": "#f57900",
+    "color6":  "#89b6e2",
+    "color14": "#46a4ff",
+    "color7":  "#cccccc",
+    "color15": "#ffffff"
+    }
+
+def get_colours(file_name, thumb_size):
+    img=Image.open(file_name)
+    img.thumbnail(thumb_size)
+    img=np.asarray(img)/255.
+    img=color.rgb2lab(img)
+    colours=[]
+    for i in range(len(img)):
+        for j in range(len(img[i])):
+            colours.append((img[i][j][0], img[i][j][1], img[i][j][2]))
+    return colours
+
+def k_means_to_np_array(k_means_out):
+    out_array=np.zeros((1, len(k_means_out), 3))
+    for i in range(len(k_means_out)):
+        for j in range(3):
+            out_array[0][i][j]=k_means_out[i][j]
+    return out_array
+
+def extract_colours_from_image(file_name, num_colours, img_sample_size, min_distance=1):
+    outed=k_means_to_np_array(k_means(get_colours(file_name, img_sample_size), num_colours, min_distance))
+    outed=color.lab2rgb(outed)
+    return_colours=[]
+    for i in range(len(outed)):
+        for j in range(len(outed[i])):
+            return_colours.append([])
+            for k in range(len(outed[i][j])):
+                return_colours[j].append(int(np.round(outed[i][j][k]*255)))
+    return return_colours
+
+def scale_colours(colours, minval, maxval): 
+    hcolours=[]
+    for colour in colours:
+        hcolours.append(rgb2hsv(colour))
+    minv=min(hcolours, key=lambda x:x[2])[2]
+    maxv=max(hcolours, key=lambda x:x[2])[2]
+    for i in range(len(hcolours)):
+        hcolours[i][2]=scale(hcolours[i][2], minv, maxv, minval, maxval)
+    scaled_colours=[]
+    for colour in hcolours:
+        scaled_colours.append(hsv2rgb(colour))
+    return scaled_colours
+
+
+def rgb2hsv(colour):
+    r=colour[0]/255.
+    g=colour[1]/255.
+    b=colour[2]/255.
+    cmax=max(r, max(g, b))
+    cmin=min(r, min(g, b))
+    delta=cmax-cmin
+    if delta==0: h=0
+    else:
+        if cmax==r: h=60.*(((g-b)/delta)%6.)
+        elif cmax==g: h=60.*((b-r)/delta+2.)
+        else: h=60.*((r-g)/delta+4.)
+
+    if cmax==0: s=0.
+    else: s=delta/cmax
+
+    v=cmax
+    return [h, s, v]
+
+def find_closest(hex_col, colours):
+    closest_dist=float('inf')
+    closest=None
+    for colour in colours:
+        dist=distance(hex2rgb(hex_col), colour)
+        if dist<closest_dist:
+            closest_dist=dist
+            closest=colour
+    return closest
+
+def hsv2rgb(colour):
+    h, s, v=colour
+    c=v*s
+    x=c*(1-abs((h/60.)%2.-1))
+    m=v-c
+    if h>=0 and h<60.:
+        r=c
+        g=x
+        b=0
+    elif h>=60. and h<120.:
+        r=x
+        g=c
+        b=0
+    elif h>=120. and h<180.:
+        r=0
+        g=c
+        b=x
+    elif h>=180. and h<240.:
+        r=0
+        g=x
+        b=c
+    elif h>=240. and h<300.:
+        r=x
+        g=0
+        b=c
+    else:
+        r=c
+        g=0
+        b=x
+    return [int(round((r+m)*255.)), int(round((g+m)*255.)), int(round((b+m)*255.))]
+
+def get_hue(colour):
+    r, g, b = colour
+    mini = min(r, min(g, b))
+    maxi = max(r, max(g, b))
+    if (maxi==mini): return int(round(360*r/255.))
+    if maxi==r: hue = (g-b)/(maxi-mini)
+    elif maxi==g: hue = 2.+(b-r)/(maxi-mini)
+    else: hue = 4. + (r-g)/(maxi-mini)
+    hue*=60
+    if hue<0: hue+=360
+    return int(round(hue))
+
+def scale(v, vmin, vmax, mmin, mmax):
+    return ((v-vmin)/(vmax-vmin)*(mmax-mmin)+mmin)
+
+def hex2rgb(hex_string):
+    return list(int(hex_string.lstrip('#')[i:i+2], 16) for i in (0, 2, 4))
+
+def rgb2hex(colour):
+    return "#%02x%02x%02x" % tuple(i for i in colour)
+
+def prepare_for_terminal(file_name, separate_bnw=False):
+    nums=16
+    #if separate_bnw: nums=18
+    colours=scale_colours(extract_colours_from_image(file_name, nums, (200, 200), 1), 0.3, 0.95)
+    colours.sort(key=lambda x: rgb2hsv(x)[2])
+    if separate_bnw:
+        background=tuple(int(round(i)) for i in average_points([colours[0], (0, 0, 0)]))
+        background=colours[0]
+        foreground=colours[-1]
+    else:
+        background=colours[0]
+        foreground=colours[-1] 
+    colour_dict={}
+    colour_dict['foreground']=rgb2hex(foreground)
+    colour_dict['background']=rgb2hex(background)
+    for key in orig_term_colours:
+        colour_dict[key]=rgb2hex(colours.pop(colours.index(find_closest(orig_term_colours[key], colours))))
+    return colour_dict
+
+def prepare_nice(file_name):
+    nums=16
+    colours=scale_colours(extract_colours_from_image(file_name, nums, (200, 200), 1), 0.3, 0.95)
+    colours.sort(key=lambda x: rgb2hsv(x)[2])
+    new_colours=[]
+    for i in range(len(colours)):
+        if i%2==0 and i<len(colours)-1:
+            c1s=0
+            c2s=0
+            for j in range(len(colours[i])):
+                c1s+=colours[i][j]**2
+                c2s+=colours[i+1][j]**2
+            if c1s<c2s:
+                new_colours.append(colours[i])
+                new_colours.append(colours[i+1])
+            else:
+                new_colours.append(colours[i+1])
+                new_colours.append(colours[i])
+    return new_colours
+
+def prepare_pypallet(file_name):
+    colours=prepare_for_terminal(file_name)
+    colour_dict={}
+    colour_dict['colour_fg']=colours['foreground']
+    colour_dict['colour_bg']=colours['background']
+    for i in range(16):
+        colour_dict['colour_{0}'.format(i)]=colours['color{0}'.format(i)]
+    return colour_dict
+
+def prepare_pypallet_9(file_name):
+    colours=scale_colours(extract_colours_from_image(file_name, 9, (200, 200), 1), 0.3, 0.95)
+    colours.sort(key=lambda x: rgb2hsv(x)[2])
+    colour_dict={}
+    colour_dict['colour_bg']=rgb2hex(colours.pop(0))
+    for i in range(len(colours)):
+        colour_dict['colour_{0}'.format(i)]=rgb2hex(colours[i])
+    return colour_dict
